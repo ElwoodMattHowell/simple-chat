96cc17ef2b83fdf14e0b9481b173c8598ae7e5ca
Matt Howell <elwhwl@gmail.com>
2022-03-25 19:49:25 -0400

    add GiftedChat

--- components/Chat.js
+++ components/Chat.js
@@ -1,21 +1,78 @@
 import React from 'react';
-import { View, StyleSheet } from 'react-native';
+import { View, Platform, KeyboardAvoidingView } from 'react-native';
+import { Bubble, GiftedChat } from 'react-native-gifted-chat'
 
 export default class Chat extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      messages: []
+    };
+  }
 
   //set screen title to props.name
   componentDidMount() {
     let name = this.props.route.params.name;
     this.props.navigation.setOptions({ title: name });
+    this.setState({
+      messages: [
+        {
+          _id: 1,
+          text: "Hello Developer",
+          createdAt: new Date(),
+          user: {
+            _id: 2,
+            name: "React Native",
+            avatar: "https://placeimg.com/140/140/any"
+          }
+        },
+        {
+          _id: 2,
+          text: "You have entered a chat",
+          createdAt: new Date(),
+          system: true
+        }
+      ]
+    });
+  }
+
+  onSend(messages = []) {
+    this.setState((previousState) => ({
+      messages: GiftedChat.append(previousState.messages, messages)
+    }))
+  }
+
+  renderBubble(props) {
+    return (
+      <Bubble
+        {...props}
+        wrapperStyle={{
+          right: {
+            backgroundColor: '#000'
+          }
+        }}
+      />
+    )
   }
 
   render() {
     let backgroundColor = this.props.route.params.backgroundColor
     return (
-      <View style={{ backgroundColor: backgroundColor, height: '100%' }}></View>
+      <View style={{ backgroundColor: backgroundColor, height: '100%' }} >
+        <GiftedChat
+          rendeBubble={this.renderBubble.bind(this)}
+          messages={this.state.messages}
+          onSend={(messages) => this.onSend(messages)}
+          user={{
+            _id: 1
+          }}
+        />
+        {Platform.OS === 'android' ? <KeyboardAvoidingView behavior="height" /> : null}
+      </View>
     )
   }
 }
+
 
 
 
